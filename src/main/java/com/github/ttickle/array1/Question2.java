c4acb8bf30f281cefeebbce5479555345b6f3a11
Thomas Tickle <ttickle@gmail.com>
2026-06-08 14:52:13 -0400

    A slightly more compact version of the logic in shuffle relying on index arithmetic.

--- src/main/java/com/github/ttickle/array1/Question2.java
+++ src/main/java/com/github/ttickle/array1/Question2.java
@@ -7,23 +7,19 @@ public class Question2 implements Solution {
     /**
      * A method of shuffling an array starting at the midpoint specified by n.  This method does not do bounds
      * checking or uneven halfs.
+     *
      * @param nums array to shuffle
-     * @param n midpoint to use for the other hand
+     * @param n    midpoint to use for the other hand
      * @return an array that shuffled the end points.
      */
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[nums.length];
 
-        int i = 0;
-        int j = n;
-        int k = 0;
-
-        while (i < n) {
-            result[k++] = nums[i];
-            result[k++] = nums[j];
-            i++;
-            j++;
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
         }
+
         return result;
     }
 }
