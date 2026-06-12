852738dc79111d67aaa4a38b2b43aa799e0f91b3
Thomas Tickle <ttickle@gmail.com>
2026-06-11 21:20:12 -0400

    Queue Question 2 optimized.

--- src/main/java/com/github/ttickle/queue/QueueQuestion2.java
+++ src/main/java/com/github/ttickle/queue/QueueQuestion2.java
@@ -3,20 +3,12 @@ package com.github.ttickle.queue;
 public class QueueQuestion2 {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int time = 0;
-        boolean finished = false;
-        while (!finished) {
-            for (int i = 0; i < tickets.length; i++) {
-                if (tickets[i] == 0) {
-                    continue;
-                }
 
-                tickets[i] -= 1;
-                time += 1;
-
-                if (tickets[k] == 0) {
-                    finished = true;
-                    break;
-                }
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time += Math.min(tickets[i], tickets[k]);
+            } else {
+                time += Math.min(tickets[i], tickets[k] - 1);
             }
         }
         return time;
