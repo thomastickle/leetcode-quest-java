d2be2cf9c2fb6905e2cbdca16aa784e736533144
Thomas Tickle <ttickle@gmail.com>
2026-06-11 20:54:15 -0400

    Further optimization of Queue: Question 1 because we really only care that the sandwich on top is needed by at least 1 student to make progress.

--- src/main/java/com/github/ttickle/queue/QueueQuestion1.java
+++ src/main/java/com/github/ttickle/queue/QueueQuestion1.java
@@ -2,27 +2,21 @@ package com.github.ttickle.queue;
 
 public class QueueQuestion1 {
     public int countStudents(int[] students, int[] sandwiches) {
-        int sandwichPointer = 0;
-        int noSandwichTaken = students.length;
-        while (sandwichPointer < sandwiches.length) {
-            boolean sandwichTaken = false;
-            for (int i = 0; i < students.length && sandwichPointer < sandwiches.length; i++) {
-                if (students[i] == -1) {
-                    continue;
-                }
+        int[] counts = new int[2];
 
-                if (students[i] == sandwiches[sandwichPointer]) {
-                    sandwichTaken = true;
-                    students[i] = -1;
-                    sandwichPointer++;
-                    noSandwichTaken--;
-                }
-            }
-            if (!sandwichTaken) {
+        for (int student : students) {
+            counts[student]++;
+        }
+
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) {
                 break;
             }
+
+            counts[sandwich]--;
         }
-        return noSandwichTaken;
+
+        return counts[0] + counts[1];
     }
 
 }
